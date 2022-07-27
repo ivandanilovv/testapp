2b6339654a678023c0ebaf6dcfc92ee9f9af8770
Ivan <ivandanilov282@gmail.com>
2022-07-27 11:20:06 +0200

    Init

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <button @click="getApi" :disabled="loading">
+      Click me!
+    </button>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      loading: false,
+      error: ''
+    }
+  },
+  methods: {
+    async getApi(){
+      this.error = ''
+      try{
+        this.loading = true;
+        const data = await this.$axios.$get('/metadata/TheAdventuresOfTomSawyer_201303');
+        console.log(data)
+      } catch (e){
+        this.error = 'ERROR! Please try again!'
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+}
+</script>
